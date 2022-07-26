f52a506fdf04529e1f1e61cc84d86563e1250fe0
Gian Bertoldi <bertoldi555@icloud.com>
2022-07-25 21:56:22 -0300

    Tentativas
    tentativas de usar metodos dentro de vetores

--- MetadeDoAnterior/src/metadedoanterior/MetadeDoAnterior.java
+++ MetadeDoAnterior/src/metadedoanterior/MetadeDoAnterior.java
@@ -5,52 +5,46 @@ import java.util.Scanner;
 
 public class MetadeDoAnterior {
 
-    private static int x;
-    private static int y;
-    
-    static int tam(int x){
-        Scanner key = new Scanner(System.in);
-        x = key.nextInt();
-        return x;
+    static String telaTam(){
+        return "Qual o TAMANHO do vetor";
     }
-    static int ent(int y){
-        Scanner key = new Scanner(System.in);
-        y = key.nextInt();
-        return y;
+    static String telaEnt(){
+        return "Qual o valor para formula?";
     }
-    static double cond (double vet){
-        int ini, cont = 2;
-        ini = tam(x);
-        double vetor;
-        vetor = ent(y);
-        double v[] = new double[ini],temp = 0;
+    static int tam(int tam){
+        return tam;
+    }
+    static double entrada(double entr){
+        return entr;
+    }
+    public static void main(String[] args) {
+        Scanner key = new Scanner(System.in);
+        System.out.println(telaTam());
+        tam(key.nextInt());
         
-        if (vetor > 0) {
+        System.out.println(telaEnt());
+        entrada(key.nextDouble());
+        
+        /*if (y > 0) {
+            v[0] = y;
             System.out.println("v[0] = "  + v[0]);
-            if (ini > 1) {
-                for (int i = 1; i != (ini - 1); i++) {
-                    if (v[0] == vetor) {
+            if (tam > 1) {
+                for (int i = 1; i != (x - 1); i++) {
+                    if (v[0] == y) {
                         v[i] = v[0]/2;
                         v[0] = -2;
                         System.out.println("v[1] = " + v[i]);
                         temp = v[i];
                     }
-                    v[i] = temp/2;
-                    temp = v[i];
-                    System.out.println("v[" + cont + "] = " + v[i]);
-                    cont++;
+                v[i] = temp/2;
+                temp = v[i];
+                System.out.println("v[" + cont + "] = " + v[i]);
+                cont++;
                 }
             }
-        } else { 
+            
+        }else{
             System.out.println("-");
-        }
-        return vet;
-    }    
-    
-    public static void main(String[] args) {
-        System.out.println("Qual o TAMANHO do vetor");
-        System.out.println("Qual o valor para formula?"); 
-        //String tela = cond(vet);
+        }*/
     }
-    
 }
