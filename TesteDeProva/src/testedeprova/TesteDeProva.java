4685ac0ba39224ca5334544f7eea284cdf311867
Gian Bertoldi <65470390+gianbertoldi@users.noreply.github.com>
2023-03-23 02:06:44 -0300

    Atualizar o TesteDeProva.java

--- TesteDeProva/src/testedeprova/TesteDeProva.java
+++ TesteDeProva/src/testedeprova/TesteDeProva.java
@@ -14,27 +14,15 @@ public class TesteDeProva {
         switch (ext) {
             case 1:
                 int[] vetor = new int[5];
-                int soma = 0;
-                
-                vetor[1] = key.nextInt();
-                vetor[2] = key.nextInt();
-                vetor[3] = key.nextInt();
-                vetor[4] = key.nextInt();
         
-                for (int i = 0; i < 4; i++){
+                for (int i = 0; i =< 3; i++){
                     vetor[i] = key.nextInt();
                     vetor[4] += vetor[i];
                 }
                 vetor[4] /= 4;
                 System.out.println(vetor[4]);
                 System.out.println("Terminou");
-            break;
-            
-            
-            
-        }
-        
-        
-    }
-    
+            break;            
+        }    
+    }    
 }
